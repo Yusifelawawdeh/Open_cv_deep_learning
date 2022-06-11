b4e1f67cdb8a5ac0ec3287971334aaf723c46e12
yusife <yusif.elawawdeh@tamu.edu>
2022-06-11 14:36:56 -0500

    initial

--- 02_01/image_END.py
+++ 02_01/image_END.py
@@ -1,7 +1,15 @@
 import numpy as np
 import cv2
+
 img = cv2.imread('../images/devon.jpg')
 #print(img.shape)
+
+b = img[:,:,0]
+g = img[:,:,1]
+r = img[:,:,2]
+cv2.imshow("blue", b)
+cv2.imshow("green", g)
+cv2.imshow("red", r)
 
 cv2.imshow('Image', img)
 cv2.waitKey(0)
